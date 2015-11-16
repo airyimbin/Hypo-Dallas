94fcdc20085d11d2a1acebf106498a4762c8dbd6
Timothy Yim <airyimbin@gmail.com>
2015-11-16 12:24:36 -0500

    removed comment

--- Hypo.cs
+++ Hypo.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: OneStop.onestop
-// Assembly: OneStop, Version=0.38.2.0, Culture=neutral, PublicKeyToken=null
-// MVID: 48A1B5F1-121D-4610-ADF6-E570367C4DBE
-// Assembly location: C:\Users\airyimbin\Desktop\OneStop.exe
-
-//
+﻿
 using Microsoft.Win32;
 using Hypo.Properties;
 using STS_Solution;
